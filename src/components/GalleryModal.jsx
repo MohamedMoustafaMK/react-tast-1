16de8e1c958b59b1afbfcaf1638bea0bfffde1c4
Mohamed Moustafa <mohamed.moustafa.mmmk@gmail.com>
2024-03-06 14:49:25 +0200

    add galleryModal -add modal in pdp to display compound photos

---
+++ src/components/GalleryModal.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { IoIosClose } from 'react-icons/io'
+import { FaArrowLeft } from 'react-icons/fa'
+import { FaArrowRight } from 'react-icons/fa'
+import '../styles/ProductDetailsPage.css'
+
+const YourComponent = ({ data, language }) => {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [imgIndex, setImgIndex] = useState(0)
+
+	const openModal = () => {
+		setIsModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false)
+		setImgIndex(0)
+	}
+
+	const handleImageNext = () => {
+		setImgIndex(imgIndex + 1)
+		if (imgIndex === data.length - 1) {
+			setImgIndex(0)
+		}
+	}
+	const handleImagePrev = () => {
+		setImgIndex(imgIndex - 1)
+		if (imgIndex === 0) {
+			setImgIndex(data.length - 1)
+		}
+	}
+
+	return (
+		<div>
+			<button onClick={openModal}>Open Modal</button>
+
+			<div
+				className={`modal-overlay ${isModalOpen ? 'modal-open' : 'modal-close'}`}
+				onClick={closeModal}
+			>
+				<div
+					className={`modal ${language === 'ar' ? 'ar' : 'en'}`}
+					onClick={(e) => e.stopPropagation()}
+				>
+					<div className='modal-close-btn-container'>
+						<IoIosClose onClick={closeModal} size={50} />
+					</div>
+					{data && data.length > 0 && (
+						<div className='modal-content-container'>
+							<img
+								className='modal-img'
+								src={data[imgIndex].image_path}
+								alt='img'
+							/>
+							<div className='overlay-gallery-btns'>
+								<div
+									className='gallery-btn'
+									onClick={handleImagePrev}
+								>
+									<FaArrowLeft />
+								</div>
+								<div
+									className='gallery-btn'
+									onClick={handleImageNext}
+								>
+									<FaArrowRight />
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default YourComponent
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,6 +1,7 @@
 import PDPCardRibbon from '../components/PDPCardRibbon'
 import PDPCarousel from '../components/PDPCarousel'
 import NavigationRibbon from '../components/NavigationRibbon'
+import GalleryModal from '../components/GalleryModal'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
@@ -8,7 +9,7 @@ const ProductDetailsPage = () => {
 	const [fetchedData, setFetchedData] = useState({})
 	const baseUrl = 'https://webapi.cooingestate.com/api/compounds/'
 	//data.cover-image-path
-	//data.compound_images.image-path
+	//data.compound_images.image_path
 	const fetchData = async (compound_id) => {
 		const { data } = await axios.get(
 			`${baseUrl}${compound_id}?token=undefined&language=en`,
@@ -17,13 +18,14 @@ const ProductDetailsPage = () => {
 		setFetchedData(data)
 	}
 	useEffect(() => {
-		fetchData(202)
+		fetchData(530)
 	}, [])
 	return (
 		<div className='page-container'>
 			<NavigationRibbon />
-			{fetchData && <PDPCarousel data={fetchedData} />}
+			{fetchedData && <PDPCarousel data={fetchedData} />}
 			<PDPCardRibbon />
+			{fetchedData && <GalleryModal data={fetchedData.compound_images} />}
 		</div>
 	)
 }
